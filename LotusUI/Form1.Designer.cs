e24be93694465333cc65d083158ebacfbce079c1
fsmateo <mateo.ferdinand@gmail.com>
2020-03-20 13:16:56 -0700

    Slight changes to Destination GridBox

--- LotusUI/Form1.Designer.cs
+++ LotusUI/Form1.Designer.cs
@@ -38,11 +38,9 @@
             this.forwardB = new System.Windows.Forms.Button();
             this.leftB = new System.Windows.Forms.Button();
             this.destGB = new System.Windows.Forms.GroupBox();
-            this.sendB = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.sendCoordinateButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.lonTB = new System.Windows.Forms.TextBox();
-            this.latTB = new System.Windows.Forms.TextBox();
+            this.coordinateTextBox = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.manualConGB.SuspendLayout();
             this.destGB.SuspendLayout();
@@ -52,11 +50,9 @@
             // 
             this.groupBox1.Controls.Add(this.serialportCB);
             this.groupBox1.Controls.Add(this.connectB);
-            this.groupBox1.Location = new System.Drawing.Point(24, 23);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.groupBox1.Size = new System.Drawing.Size(358, 102);
+            this.groupBox1.Size = new System.Drawing.Size(179, 53);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Serial Connection";
@@ -65,18 +61,16 @@
             // 
             this.serialportCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.serialportCB.FormattingEnabled = true;
-            this.serialportCB.Location = new System.Drawing.Point(186, 38);
-            this.serialportCB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.serialportCB.Location = new System.Drawing.Point(93, 20);
             this.serialportCB.Name = "serialportCB";
-            this.serialportCB.Size = new System.Drawing.Size(156, 33);
+            this.serialportCB.Size = new System.Drawing.Size(80, 21);
             this.serialportCB.TabIndex = 1;
             // 
             // connectB
             // 
-            this.connectB.Location = new System.Drawing.Point(12, 37);
-            this.connectB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.connectB.Location = new System.Drawing.Point(6, 19);
             this.connectB.Name = "connectB";
-            this.connectB.Size = new System.Drawing.Size(160, 44);
+            this.connectB.Size = new System.Drawing.Size(80, 23);
             this.connectB.TabIndex = 0;
             this.connectB.Text = "Connect";
             this.connectB.UseVisualStyleBackColor = true;
@@ -88,21 +82,18 @@
             this.manualConGB.Controls.Add(this.reverseB);
             this.manualConGB.Controls.Add(this.forwardB);
             this.manualConGB.Controls.Add(this.leftB);
-            this.manualConGB.Location = new System.Drawing.Point(24, 365);
-            this.manualConGB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.manualConGB.Location = new System.Drawing.Point(12, 163);
             this.manualConGB.Name = "manualConGB";
-            this.manualConGB.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.manualConGB.Size = new System.Drawing.Size(358, 244);
+            this.manualConGB.Size = new System.Drawing.Size(179, 125);
             this.manualConGB.TabIndex = 1;
             this.manualConGB.TabStop = false;
             this.manualConGB.Text = "Manual Control";
             // 
             // rightB
             // 
-            this.rightB.Location = new System.Drawing.Point(236, 112);
-            this.rightB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.rightB.Location = new System.Drawing.Point(118, 58);
             this.rightB.Name = "rightB";
-            this.rightB.Size = new System.Drawing.Size(110, 48);
+            this.rightB.Size = new System.Drawing.Size(55, 25);
             this.rightB.TabIndex = 3;
             this.rightB.Text = "Right";
             this.rightB.UseVisualStyleBackColor = true;
@@ -110,10 +101,9 @@
             // 
             // reverseB
             // 
-            this.reverseB.Location = new System.Drawing.Point(126, 185);
-            this.reverseB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.reverseB.Location = new System.Drawing.Point(63, 96);
             this.reverseB.Name = "reverseB";
-            this.reverseB.Size = new System.Drawing.Size(110, 48);
+            this.reverseB.Size = new System.Drawing.Size(55, 25);
             this.reverseB.TabIndex = 1;
             this.reverseB.Text = "Reverse";
             this.reverseB.UseVisualStyleBackColor = true;
@@ -121,10 +111,9 @@
             // 
             // forwardB
             // 
-            this.forwardB.Location = new System.Drawing.Point(126, 37);
-            this.forwardB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.forwardB.Location = new System.Drawing.Point(63, 19);
             this.forwardB.Name = "forwardB";
-            this.forwardB.Size = new System.Drawing.Size(110, 48);
+            this.forwardB.Size = new System.Drawing.Size(55, 25);
             this.forwardB.TabIndex = 0;
             this.forwardB.Text = "Forward";
             this.forwardB.UseVisualStyleBackColor = true;
@@ -132,10 +121,9 @@
             // 
             // leftB
             // 
-            this.leftB.Location = new System.Drawing.Point(12, 112);
-            this.leftB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.leftB.Location = new System.Drawing.Point(6, 58);
             this.leftB.Name = "leftB";
-            this.leftB.Size = new System.Drawing.Size(110, 48);
+            this.leftB.Size = new System.Drawing.Size(55, 25);
             this.leftB.TabIndex = 2;
             this.leftB.Text = "Left";
             this.leftB.UseVisualStyleBackColor = true;
@@ -143,78 +131,52 @@
             // 
             // destGB
             // 
-            this.destGB.Controls.Add(this.sendB);
-            this.destGB.Controls.Add(this.label2);
+            this.destGB.Controls.Add(this.sendCoordinateButton);
             this.destGB.Controls.Add(this.label1);
-            this.destGB.Controls.Add(this.lonTB);
-            this.destGB.Controls.Add(this.latTB);
-            this.destGB.Location = new System.Drawing.Point(24, 137);
-            this.destGB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.destGB.Controls.Add(this.coordinateTextBox);
+            this.destGB.Location = new System.Drawing.Point(12, 71);
             this.destGB.Name = "destGB";
-            this.destGB.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.destGB.Size = new System.Drawing.Size(358, 217);
+            this.destGB.Size = new System.Drawing.Size(179, 86);
             this.destGB.TabIndex = 2;
             this.destGB.TabStop = false;
             this.destGB.Text = "Destination";
             // 
-            // sendB
+            // sendCoordinateButton
             // 
-            this.sendB.Location = new System.Drawing.Point(20, 137);
-            this.sendB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.sendB.Name = "sendB";
-            this.sendB.Size = new System.Drawing.Size(150, 44);
-            this.sendB.TabIndex = 4;
-            this.sendB.Text = "Send";
-            this.sendB.UseVisualStyleBackColor = true;
-            this.sendB.Click += new System.EventHandler(this.sendB_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 92);
-            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(107, 25);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Longitude";
+            this.sendCoordinateButton.Location = new System.Drawing.Point(10, 58);
+            this.sendCoordinateButton.Name = "sendCoordinateButton";
+            this.sendCoordinateButton.Size = new System.Drawing.Size(75, 23);
+            this.sendCoordinateButton.TabIndex = 4;
+            this.sendCoordinateButton.Text = "Send";
+            this.sendCoordinateButton.UseVisualStyleBackColor = true;
+            this.sendCoordinateButton.Click += new System.EventHandler(this.sendB_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 42);
-            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Location = new System.Drawing.Point(7, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 25);
+            this.label1.Size = new System.Drawing.Size(95, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Latitude";
+            this.label1.Text = "Latitude,Longitude";
             // 
-            // lonTB
+            // coordinateTextBox
             // 
-            this.lonTB.Location = new System.Drawing.Point(126, 87);
-            this.lonTB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.lonTB.Name = "lonTB";
-            this.lonTB.Size = new System.Drawing.Size(196, 31);
-            this.lonTB.TabIndex = 1;
-            // 
-            // latTB
-            // 
-            this.latTB.Location = new System.Drawing.Point(126, 37);
-            this.latTB.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.latTB.Name = "latTB";
-            this.latTB.Size = new System.Drawing.Size(196, 31);
-            this.latTB.TabIndex = 0;
+            this.coordinateTextBox.Location = new System.Drawing.Point(10, 32);
+            this.coordinateTextBox.Name = "coordinateTextBox";
+            this.coordinateTextBox.Size = new System.Drawing.Size(163, 20);
+            this.coordinateTextBox.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1108, 633);
+            this.ClientSize = new System.Drawing.Size(554, 292);
             this.Controls.Add(this.destGB);
             this.Controls.Add(this.manualConGB);
             this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Lotus 2.0";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -238,11 +200,9 @@
         private System.Windows.Forms.Button forwardB;
         private System.Windows.Forms.Button leftB;
         private System.Windows.Forms.GroupBox destGB;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox latTB;
-        private System.Windows.Forms.TextBox lonTB;
-        private System.Windows.Forms.Button sendB;
+        private System.Windows.Forms.TextBox coordinateTextBox;
+        private System.Windows.Forms.Button sendCoordinateButton;
     }
 }
 
